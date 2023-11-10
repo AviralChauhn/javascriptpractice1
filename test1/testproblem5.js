b16307dabb9c6e0c07092586ded98fa25904d463
Aviral <chauhanaviral79@gmail.com>
2023-11-10 12:30:15 +0530

    initial commit

--- test1/testproblem5.js
+++ test1/testproblem5.js
@@ -2,4 +2,4 @@ const { inventory } = require("../data")
 const {age}=require("../problem4")
 const{getcarage}=require("../problem5")
 const arr =age(inventory);
-const agecar = getcarage(arr)
+const agecar = getcarage(inventory,arr)
